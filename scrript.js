e6cedbb19a2f39f238cb9b593ca9ed22c8e73758
iQuizeo <132915189+iQuizeo@users.noreply.github.com>
2023-09-27 15:26:13 +0530

    Update scrript.js

--- scrript.js
+++ scrript.js
@@ -3,25 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const checkboxes = form.querySelectorAll("input[type='checkbox']");
     const selectedCandidatesList = form.querySelector("ul#selectedCandidates");
 
+    // Retrieve stored poll data if it exists
+    const storedPollData = JSON.parse(localStorage.getItem('pollData')) || [];
+
+    // Pre-select checkboxes based on stored data
+    checkboxes.forEach(checkbox => {
+        if (storedPollData.includes(checkbox.value)) {
+            checkbox.checked = true;
+        }
+    });
+
+    updateSelectedCandidatesList();
+
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-
-        // Count the number of selected candidates
         const selectedCandidates = Array.from(checkboxes)
             .filter(candidate => candidate.checked)
             .map(candidate => candidate.value);
 
-        // Check if the number of selected candidates is not more than 8
         if (selectedCandidates.length <= 8) {
-            // Display the selected candidates in the "Your Ballot" section
-            selectedCandidatesList.innerHTML = "";
-            selectedCandidates.forEach(candidate => {
-                const listItem = document.createElement("li");
-                listItem.textContent = candidate;
-                selectedCandidatesList.appendChild(listItem);
-            });
+            // Store the selected candidates in localStorage
+            localStorage.setItem('pollData', JSON.stringify(selectedCandidates));
+
+            // Update the displayed selected candidates
+            updateSelectedCandidatesList();
         } else {
             alert("You selected more than 8 candidates. Please select up to 8 candidates.");
         }
     });
+
+    function updateSelectedCandidatesList() {
+        // Display the selected candidates in the "Your Ballot" section
+        selectedCandidatesList.innerHTML = "";
+        const storedCandidates = JSON.parse(localStorage.getItem('pollData')) || [];
+        storedCandidates.forEach(candidate => {
+            const listItem = document.createElement("li");
+            listItem.textContent = candidate;
+            selectedCandidatesList.appendChild(listItem);
+        });
+    }
 });
